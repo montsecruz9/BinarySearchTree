94621ec6947e0d515072509491caeabf22335b35
Montserrat Cruz <104812189+montsecruz9@users.noreply.github.com>
2024-01-25 16:10:43 -0600

    Added get method
    Added get method with examples that search for some numbers

--- Main.java
+++ Main.java
@@ -13,8 +13,15 @@ public class Main {
         intTree.insert(26);
         intTree.insert(22);
         intTree.insert(32);
+        intTree.insert(17);
 
         intTree.traverseInOrder();
+
+        System.out.println();
+
+        System.out.println(intTree.get(27));
+        System.out.println(intTree.get(17));
+        System.out.println(intTree.get(888));
 
 
     }
