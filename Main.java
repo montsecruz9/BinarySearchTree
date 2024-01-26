ddaa3bb79768871c4d8eeefa6bf1ad90f68b99a5
Montserrat Cruz <104812189+montsecruz9@users.noreply.github.com>
2024-01-26 12:44:48 -0600

    Updated code
    testing delete method

--- Main.java
+++ Main.java
@@ -15,17 +15,22 @@ public class Main {
         intTree.insert(32);
         intTree.insert(17);
 
-//        intTree.traverseInOrder();
-//
-//        System.out.println();
+        intTree.traverseInOrder();
+        System.out.println();
 //
 //        System.out.println(intTree.get(27));
 //        System.out.println(intTree.get(17));
 //        System.out.println(intTree.get(888));
 
-        System.out.println(intTree.min());
-        System.out.println(intTree.max());
+//        System.out.println(intTree.min());
+//        System.out.println(intTree.max());
 
+//        intTree.delete(15);
+//        intTree.delete(17);
+        intTree.delete(25);
+
+        intTree.traverseInOrder();
+        System.out.println();
 
     }
 }
